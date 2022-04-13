76ead36c46da4721454f0dca92247f18fe6e5960
deana <dvandegriff1@gmail.com>
2022-04-13 10:26:13 -0500

    adds RESTful Routes and API Actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/places" => "places#index"
+  post "/places" => "places#create"
+  get "/places/:id" => "places#show"
+  patch "/places/:id" => "places#update"
+  delete "/places/:id" => "places#destroy"
 end
